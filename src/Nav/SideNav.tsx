7f315ef65b5984d27af6f810b89b37f5459361ae
Roharui <jack555586@gmail.com>
2020-11-05 04:38:26 +0000

    SideNav CSS 수정

--- src/Nav/SideNav.tsx
+++ src/Nav/SideNav.tsx
@@ -8,9 +8,11 @@ class SideNav extends React.Component<{toggle:boolean}, {}> {
       return <>
         <div id="mySidenav" className="sidenav" style={{width: this.props.toggle ? "250px" : "0px"}}>
             <div className="profile">
-                <img src="https://seoulforest.or.kr/wordpress/wp-content/uploads/2017/09/20171031_065922.jpg" alt="프로필" />
-                <div style={{color:"white"}}><h3>Roharui</h3></div>
-                <div>
+                <div className="pro-img">
+                    <img src="https://seoulforest.or.kr/wordpress/wp-content/uploads/2017/09/20171031_065922.jpg" alt="프로필" />
+                    <h3>Roharui</h3>
+                </div>
+                <div className="w-info">
                     <span>추천수 : <span className="num rcmd">100</span></span>
                     <span>검증글 : <span className="num vfi">100</span></span>
                 </div>
