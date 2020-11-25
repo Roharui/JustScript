6d696a0ea025787230385c807563ce998f5a2dc7
Roharui <jack555586@gmail.com>
2020-11-25 20:54:45 +0900

    글 쓰기 버튼 추가

--- src/Creater/Creater.tsx
+++ src/Creater/Creater.tsx
@@ -1,15 +1,34 @@
 
 import React from 'react';
 import DataSender from '../DataSender/DataSender'
+import Canvas from '../Main/Item/Canvas'
+import { ItemType } from '../Main/Item/Item'
 
-class Creater extends React.Component<any, any> {
+interface CreaterState{
+    session:string | null, 
+    item:ItemType
+}
+
+class Creater extends React.Component<any, CreaterState> {
     private ds:DataSender;
 
     constructor(props:any){
         super(props);
         this.ds = new DataSender();
         this.state = {
-            session : null
+            session : null,
+            item: {
+                id:-1,
+                img:"Icon.png", 
+                name:"TEST", 
+                descript:"TEST", 
+                type: "html",
+                script:"", 
+                score:0, 
+                openAble:true,
+                width: "50%",
+                height: "50%"
+            }
         }
     }
 
@@ -19,11 +38,18 @@ class Creater extends React.Component<any, any> {
             alert("You need to login!")
             this.props.history.push("/")
         }
-        this.setState({session: login})
+        this.setState({...this.state, session: login})
     }
 
     render() {
-        return <></>
+        return <>
+            <div className="preview">
+                <Canvas item={this.state.item} />
+            </div>
+            <div className="mirror">
+                <textarea style={{width:"100%", height:"100%"}}></textarea>
+            </div>
+        </>
     }
 }
 
