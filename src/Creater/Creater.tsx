bef6cd141edd4a237ab5cd2ffbfbf18ba1ae66f6
Roharui <jack555586@gmail.com>
2020-11-27 22:24:20 +0900

    Creater 기능 추가

--- src/Creater/Creater.tsx
+++ src/Creater/Creater.tsx
@@ -1,9 +1,10 @@
 
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import DataSender from '../DataSender/DataSender'
-// import Canvas from '../Main/Item/Canvas'
-import Iframe from '../Main/Item/Iframe'
 import { ItemType } from '../Main/Item/Item'
+import { Content } from '../Main/Content'
+import { Button } from '@material-ui/core';
+
 import './Creater.css'
 
 interface CreaterState{
@@ -13,6 +14,7 @@ interface CreaterState{
 
 class Creater extends React.Component<any, CreaterState> {
     private ds:DataSender;
+    private mirror: HTMLTextAreaElement;
 
     constructor(props:any){
         super(props);
@@ -23,15 +25,17 @@ class Creater extends React.Component<any, CreaterState> {
                 id:-1,
                 img:"Icon.png", 
                 name:"TEST", 
-                descript:"TEST", 
+                descript:"TEST",
                 type: "html",
-                script:"<h1>TEST</h1>", 
+                script:"", 
                 score:0, 
                 openAble:true,
                 width: "500px",
                 height: "400px"
             }
         }
+
+        this.mirror = {} as HTMLTextAreaElement
     }
 
     componentDidMount(){
@@ -43,28 +47,61 @@ class Creater extends React.Component<any, CreaterState> {
         this.setState({...this.state, session: login})
     }
 
+    changeEvent = (e:ChangeEvent<HTMLSelectElement>) => {
+        let type = e.target.value as "html" | "tema" | "canvas" | "writer"
+        let item:ItemType = {...this.state.item, type:type, script:""}
+        this.mirror.value = ""
+        this.setState({item:item})
+    }
+
+    updateScript = () => {
+        let item:ItemType = {...this.state.item, script:this.mirror.value}
+        this.setState({item:item})
+    }
+
+    changeSize = (e:ChangeEvent<HTMLInputElement>) => {
+        let item:ItemType = {...this.state.item, [e.target.name]: e.target.value}
+        this.setState({item:item})
+    }
+
     render() {
         let item = this.state.item;
         return <>
             <div className="creater">
-                <div className="controller">
-                    <label>
-                        SizeX : 
-                        <input type="text" id="sizeX" />
-                    </label>
-                    <label>
-                        SizeY : 
-                        <input type="text" id="sizeY" />
-                    </label>
-                </div>
                 <div className="preview">
                     <div className='inner' style={{width: item.width, height: item.height}}>
-                        <Iframe item={item} />
+                        {Content(item)}
                     </div>
                 </div>
                 <div className="mirror">
-                    <textarea style={{width:"100%", height:"100%"}}></textarea>
+                    <div className="controller">
+                        <label>
+                        width : 
+                            <input type="text" id="sizeX" name="width" onChange={this.changeSize} value={item.width}/>
+                        </label>
+                        <label>
+                        height : 
+                            <input type="text" id="sizeY" name="height" onChange={this.changeSize} value={item.height}/>
+                        </label>
+                        <label>
+                            Type : 
+                            <select className="ItemType" onChange={this.changeEvent}>
+                                <option value="html">Html</option>
+                                <option value="canvas">Canvas</option>
+                                <option value="tema">Tema</option>
+                            </select>
+                        </label>
+                    </div>
+                    <textarea ref={(ref:HTMLTextAreaElement) => { this.mirror = ref }} style={{width:"100%", height:"100%"}}></textarea>
                 </div>
+                <Button style={{
+                    position:"absolute", 
+                    bottom: "30px", 
+                    right: "30px", 
+                    backgroundColor: "lightblue",
+                    width: "100px",
+                    height: "40px"
+                }} onClick={this.updateScript}>UPDATE</Button>
             </div>
         </>
     }
