e58524f0aed77164e09136025f366acc16ba8871
Roharui <jack555586@gmail.com>
2020-12-03 20:58:01 +0900

    종료버튼 위치 변경

--- src/Main/Popup.tsx
+++ src/Main/Popup.tsx
@@ -15,11 +15,11 @@ export interface PopupType{
 
 export default function Popup(props:PopupType){
   let oper = props.oper;
-  let {width, height} = props.item
+  let {width, height} = !oper.writer ? props.item : {width:"50%",height:"50%"}
   return (
     <div className='popup'>
+      <Button style={{position:"fixed", top:"10px", right:"10px", backgroundColor: "white"}} onClick={oper.closer}>X</Button>
       <div className='popup_inner' style={{width: width, height: height}}>
-      <Button style={{position:"absolute", top:"10px", right:"10px", backgroundColor: "white"}} onClick={oper.closer}>X</Button>
       {oper.writer ? <Scripter item={props.item} writer={oper.writer}/> : Content(props.item)}
       </div>
     </div>
