1c353b0bb93630d03d0f1081a605825f66987e6f
Roharui <jack555586@gmail.com>
2020-12-09 22:07:09 +0900

    테스트 코드 주석처리

--- backend/router/BaseRouter.ts
+++ backend/router/BaseRouter.ts
@@ -5,9 +5,9 @@ const router:Router = express.Router();
 
 const db = new Manager();
 
-router.get("/", async (req: express.Request, res: express.Response) => {
-    let items = await db.select();
-    res.json(items)
-})
+// router.get("/", async (req: express.Request, res: express.Response) => {
+//     let items = await db.select(5);
+//     res.json(items)
+// })
 
 export default router; 
