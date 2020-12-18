acab4fce64a439696be6ceb599ac41d063c0ee67
Roharui <jack555586@gmail.com>
2020-12-18 21:44:41 +0900

    프로필 디자인 추가

--- src/Profile/index.tsx
+++ src/Profile/index.tsx
@@ -1,11 +1,41 @@
 
 import React from 'react';
+import DataSender from 'src/DataSender/DataSender';
+
+import './Profile.css'
 
 class Profile extends React.Component<any, any>{
+    private ds: DataSender;
+    
+    constructor(props:any){
+        super(props);
+        this.state = {
+            report_count:-1,
+            write_count:0,
+            recomment_count:0,
+            nickname:'',
+            profile_img:''
+        }
+        this.ds = new DataSender();
+    }
+    
+    componentDidMount(){
+        let login = sessionStorage.getItem("login")
+        if(!login) {
+            alert("You need to login!")
+            this.props.history.push("/")
+        }else{
+            this.setState({...this.state, session: login})
+        }
+    }
 
     render(){
         return <>
-            
+        <div className="content">
+            <div className="card"></div>
+            <div className="screen"></div>
+        </div>
+        <div className="bottom"></div>
         </>
     }
     
