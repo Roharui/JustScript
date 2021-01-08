64595c9486605134e81cc6b3b75e5842a59f0370
Roharui <jack555586@gmail.com>
2021-01-08 21:59:34 +0900

    추천시 갱신 불가 버그 수정

--- backend/router/Item.ts
+++ backend/router/Item.ts
@@ -5,20 +5,6 @@ const ItemManager:Router = express.Router();
 
 const db = new ItemDB();
 
-ItemManager.get("/", async (req: express.Request, res: express.Response) => {
-    let {score, session, filter} = req.query
-    let s:number = 5;
-    if(typeof score === "string"){
-        try{ s = parseInt(score);} 
-        catch(e){ s = 5; }
-    }
-
-    let f = (filter as string).split(",")
-
-    let items = await db.select(s, f, userSession[(session as string)]);
-    res.status(200).json({data:items})
-})
-
 ItemManager.post("/owner", loginChecker, async (req: express.Request, res: express.Response) => {
     const {_id} = req.body
     let items = await db.selectByUser(_id)
@@ -33,7 +19,7 @@ ItemManager.post("/insert", loginChecker, async (req: express.Request, res: expr
 
 ItemManager.post("/recommend", loginChecker, async (req: express.Request, res: express.Response) => {
     const {item_id, _id, flag} = req.body;
-    db.recommend(item_id, _id, flag)
+    await db.recommend(item_id, _id, flag)
     res.status(200).send()
 })
 
@@ -43,8 +29,22 @@ ItemManager.delete("/delete", loginChecker, async (req: express.Request, res: ex
     db.delete(id, _id)
     .then(() => res.status(200))
     .catch(() => res.status(400))
-
+    
     res.send()
+})
+
+ItemManager.get("/", async (req: express.Request, res: express.Response) => {
+    let {score, session, filter} = req.query
+    let s:number = 5;
+    if(typeof score === "string"){
+        try{ s = parseInt(score);} 
+        catch(e){ s = 5; }
+    }
+
+    let f = (filter as string).split(",")
+
+    let items = await db.select(s, f, userSession[(session as string)]);
+    res.status(200).json({data:items})
 })
 
 export default ItemManager; 
